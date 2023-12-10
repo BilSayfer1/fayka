2298281a2f88e0ac9645253879deee4f3eaa9557
BilSayfer1 <alsafirbozorov5@gmail.com>
2023-12-11 00:38:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,860 @@
+
+
+
+// // let arr = [`hasan`, `husen`, `akobir`, `chushpan`, `hasan baran`]
+// // let longestName = ''
+// // for (let name of arr) {
+// //     if (name.length > longestName.length) {
+// //         longestName = name;
+// //     }
+// // }
+// // console.log(longestName);
+
+
+
+
+// // let double_hasanbaran = []
+
+// // let double_dliniy = ''
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     if (arr[i].length > double_dliniy.length) {
+// //         double_dliniy = arr[i];
+// //     }
+// // }
+// // console.log(double_dliniy);
+
+
+// let evens = []
+
+// let numbers = [
+//     0,
+//     2,
+//     4,
+//     6,
+//     8,
+//     10,
+//     12,
+//     14,
+//     16,
+//     18,
+//     20,
+//     22,
+//     24,
+//     26,
+//     28,
+//     30,
+//     32,
+//     34,
+//     36,
+//     38,
+//     40,
+//     42,
+//     44,
+//     46,
+//     48,
+//     50,
+//     52,
+//     54,
+//     56,
+//     58,
+//     60,
+//     62,
+//     64,
+//     66,
+//     68,
+//     70,
+//     72,
+//     74,
+//     76,
+//     78,
+//     80,
+//     82,
+//     84,
+//     86,
+//     88,
+//     90,
+//     92,
+//     94,
+//     96,
+//     98,
+//     100,
+//     102,
+//     104,
+//     106,
+//     108,
+//     110,
+//     112,
+//     114,
+//     116,
+//     118,
+//     120,
+//     122,
+//     124,
+//     126,
+//     128,
+//     130,
+//     132,
+//     134,
+//     136,
+//     138,
+//     140,
+//     142,
+//     144,
+//     146,
+//     148,
+//     150,
+//     152,
+//     154,
+//     156,
+//     158,
+//     160,
+//     162,
+//     164,
+//     166,
+//     168,
+//     170,
+//     172,
+//     174,
+//     176,
+//     178,
+//     180,
+//     182,
+//     184,
+//     186,
+//     188,
+//     190,
+//     192,
+//     194,
+//     196,
+//     198,
+//     200,
+//     202,
+//     204,
+//     206,
+//     208,
+//     210,
+//     212,
+//     214,
+//     216,
+//     218,
+//     220,
+//     222,
+//     224,
+//     226,
+//     228,
+//     230,
+//     232,
+//     234,
+//     236,
+//     238,
+//     240,
+//     242,
+//     244,
+//     246,
+//     248,
+//     250,
+//     252,
+//     254,
+//     256,
+//     258,
+//     260,
+//     262,
+//     264,
+//     266,
+//     268,
+//     270,
+//     272,
+//     274,
+//     276,
+//     278,
+//     280,
+//     282,
+//     284,
+//     286,
+//     288,
+//     290,
+//     292,
+//     294,
+//     296,
+//     298,
+//     300,
+//     302,
+//     304,
+//     306,
+//     308,
+//     310,
+//     312,
+//     314,
+//     316,
+//     318,
+//     320,
+//     322,
+//     324,
+//     326,
+//     328,
+//     330,
+//     332,
+//     334,
+//     336,
+//     338,
+//     340,
+//     342,
+//     344,
+//     346,
+//     348,
+//     350,
+//     352,
+//     354,
+//     356,
+//     358,
+//     360,
+//     362,
+//     364,
+//     366,
+//     368,
+//     370,
+//     372,
+//     374,
+//     376,
+//     378,
+//     380,
+//     382,
+//     384,
+//     386,
+//     388,
+//     390,
+//     392,
+//     394,
+//     396,
+//     398,
+//     400,
+//     402,
+//     404,
+//     406,
+//     408,
+//     410,
+//     412,
+//     414,
+//     416,
+//     418,
+//     420,
+//     422,
+//     424,
+//     426,
+//     428,
+//     430,
+//     432,
+//     434,
+//     436,
+//     438,
+//     440,
+//     442,
+//     444,
+//     446,
+//     448,
+//     450,
+//     452,
+//     454,
+//     456,
+//     458,
+//     460,
+//     462,
+//     464,
+//     466,
+//     468,
+//     470,
+//     472,
+//     474,
+//     476,
+//     478,
+//     480,
+//     482,
+//     484,
+//     486,
+//     488,
+//     490,
+//     492,
+//     494,
+//     496,
+//     498,
+//     500,
+//     502,
+//     504,
+//     506,
+//     508,
+//     510,
+//     512,
+//     514,
+//     516,
+//     518,
+//     520,
+//     522,
+//     524,
+//     526,
+//     528,
+//     530,
+//     532,
+//     534,
+//     536,
+//     538,
+//     540,
+//     542,
+//     544,
+//     546,
+//     548,
+//     550,
+//     552,
+//     554,
+//     556,
+//     558,
+//     560,
+//     562,
+//     564,
+//     566,
+//     568,
+//     570,
+//     572,
+//     574,
+//     576,
+//     578,
+//     580,
+//     582,
+//     584,
+//     586,
+//     588,
+//     590,
+//     592,
+//     594,
+//     596,
+//     598,
+//     600,
+//     602,
+//     604,
+//     606,
+//     608,
+//     610,
+//     612,
+//     614,
+//     616,
+//     618,
+//     620,
+//     622,
+//     624,
+//     626,
+//     628,
+//     630,
+//     632,
+//     634,
+//     636,
+//     638,
+//     640,
+//     642,
+//     644,
+//     646,
+//     648,
+//     650,
+//     652,
+//     654,
+//     656,
+//     658,
+//     660,
+//     662,
+//     664,
+//     666,
+//     668,
+//     670,
+//     672,
+//     674,
+//     676,
+//     678,
+//     680,
+//     682,
+//     684,
+//     686,
+//     688,
+//     690,
+//     692,
+//     694,
+//     696,
+//     698,
+//     700,
+//     702,
+//     704,
+//     706,
+//     708,
+//     710,
+//     712,
+//     714,
+//     716,
+//     718,
+//     720,
+//     722,
+//     724,
+//     726,
+//     728,
+//     730,
+//     732,
+//     734,
+//     736,
+//     738,
+//     740,
+//     742,
+//     744,
+//     746,
+//     748,
+//     750,
+//     752,
+//     754,
+//     756,
+//     758,
+//     760,
+//     762,
+//     764,
+//     766,
+//     768,
+//     770,
+//     772,
+//     774,
+//     776,
+//     778,
+//     780,
+//     782,
+//     784,
+//     786,
+//     788,
+//     790,
+//     792,
+//     794,
+//     796,
+//     798,
+//     800,
+//     802,
+//     804,
+//     806,
+//     808,
+//     810,
+//     812,
+//     814,
+//     816,
+//     818,
+//     820,
+//     822,
+//     824,
+//     826,
+//     828,
+//     830,
+//     832,
+//     834,
+//     836,
+//     838,
+//     840,
+//     842,
+//     844,
+//     846,
+//     848,
+//     850,
+//     852,
+//     854,
+//     856,
+//     858,
+//     860,
+//     862,
+//     864,
+//     866,
+//     868,
+//     870,
+//     872,
+//     874,
+//     876,
+//     878,
+//     880,
+//     882,
+//     884,
+//     886,
+//     888,
+//     890,
+//     892,
+//     894,
+//     896,
+//     898,
+//     900,
+//     902,
+//     904,
+//     906,
+//     908,
+//     910,
+//     912,
+//     914,
+//     916,
+//     918,
+//     920,
+// ]
+// for(i = 0; i < 920; i++) {
+//     if(i % 2 === 0) {
+//         evens.push(i)
+//     }
+// }
+// console.log(evens);
+
+
+// let students = [
+//     {
+//         name: `Al Safir`,
+//         age: 15,
+//         gender: `m`
+//     },
+//     {
+//         name: `Khasan`,
+//         age: 7,
+//         gender: `m`
+//     },
+//     {
+//         name: `Allayor`,
+//         age: 15,
+//         gender: `w`
+//     },
+//     {
+//         name: `Radmir`,
+//         age: 19,
+//         gender: `w`
+//     },
+//     {
+//         name: `Jaxongir`,
+//         age: 17,
+//         gender: `m`
+//     },
+//     {
+//         name: `Bexruz`,
+//         age: 16,
+//         gender: `w`
+//     },
+//     {
+//         name: `Alibek`,
+//         age: 17,
+//         gender: `m` 
+//     }
+// ]
+
+// let totalage = 0
+
+// for (let i = 0; i < students.length; i++) {
+//     totalage += students[i].age;
+// }
+// let average_age = totalage / students.length
+// console.log(`средний возраст ${average_age} `);
+// let m = 0
+// let w = 0
+
+// for(let i = 0; i < students.length; i++) {
+//     if(students[i].gender === `w`) {
+//         w++
+//     } else {
+//         m++
+//     }
+// } 
+// console.log(`Люди с мужским полом  ${m} `);
+// console.log(`Люди с жеским полом  ${w}`);
+
+
+// students.forEach(student => {
+//     switch (student.gender) {
+//         case `w`:
+//             w++;
+//             break;
+//         case `m`:
+//             m++;
+//             break;
+//         default:
+//             break;
+//     }
+
+// })
+
+// console.log(`Люди с мужским полом  ${m} `);
+// console.log(`Люди с жеским полом  ${w}`)
+
+// Раскидать людей в разные массивы в зависимости от их почты
+
+let users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+            "geo": {
+                "lat": "-68.6102",
+                "lng": "-47.0653"
+            }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {
+                "lat": "29.4572",
+                "lng": "-164.2990"
+            }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263",
+            "geo": {
+                "lat": "-31.8129",
+                "lng": "62.5342"
+            }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+            "name": "Keebler LLC",
+            "catchPhrase": "User-centric fault-tolerant solution",
+            "bs": "revolutionize end-to-end systems"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Mrs. Dennis Schulist",
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "address": {
+            "street": "Norberto Crossing",
+            "suite": "Apt. 950",
+            "city": "South Christy",
+            "zipcode": "23505-1337",
+            "geo": {
+                "lat": "-71.4197",
+                "lng": "71.7478"
+            }
+        },
+        "phone": "1-477-935-8478 x6430",
+        "website": "ola.org",
+        "company": {
+            "name": "Considine-Lockman",
+            "catchPhrase": "Synchronised bottom-line interface",
+            "bs": "e-enable innovative applications"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "address": {
+            "street": "Rex Trail",
+            "suite": "Suite 280",
+            "city": "Howemouth",
+            "zipcode": "58804-1099",
+            "geo": {
+                "lat": "24.8918",
+                "lng": "21.8984"
+            }
+        },
+        "phone": "210.067.6132",
+        "website": "elvis.io",
+        "company": {
+            "name": "Johns Group",
+            "catchPhrase": "Configurable multimedia task-force",
+            "bs": "generate enterprise e-tailers"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir V",
+        "username": "Maxime_Nienow",
+        "email": "Sherwood@rosamond.me",
+        "address": {
+            "street": "Ellsworth Summit",
+            "suite": "Suite 729",
+            "city": "Aliyaview",
+            "zipcode": "45169",
+            "geo": {
+                "lat": "-14.3990",
+                "lng": "-120.7677"
+            }
+        },
+        "phone": "586.493.6943 x140",
+        "website": "jacynthe.com",
+        "company": {
+            "name": "Abernathy Group",
+            "catchPhrase": "Implemented secondary concept",
+            "bs": "e-enable extensible e-tailers"
+        }
+    },
+    {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "address": {
+            "street": "Dayna Park",
+            "suite": "Suite 449",
+            "city": "Bartholomebury",
+            "zipcode": "76495-3109",
+            "geo": {
+                "lat": "24.6463",
+                "lng": "-168.8889"
+            }
+        },
+        "phone": "(775)976-6794 x41206",
+        "website": "conrad.com",
+        "company": {
+            "name": "Yost and Sons",
+            "catchPhrase": "Switchable contextually-based project",
+            "bs": "aggregate real-time technologies"
+        }
+    },
+    {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "address": {
+            "street": "Kattie Turnpike",
+            "suite": "Suite 198",
+            "city": "Lebsackbury",
+            "zipcode": "31428-2261",
+            "geo": {
+                "lat": "-38.2386",
+                "lng": "57.2232"
+            }
+        },
+        "phone": "024-648-3804",
+        "website": "ambrose.net",
+        "company": {
+            "name": "Hoeger LLC",
+            "catchPhrase": "Centralized empowering task-force",
+            "bs": "target end-to-end models"
+        }
+    }
+]
+let emails = {
+    org: [],
+    net: [],
+    info: []
+}
+let other = [] 
+
+// for(let user of users) {
+//     let emaildomain = user.email.split(`.`).pop()
+//     if(emaildomain === `org`) {
+//         emails.org.push(user)
+//     } else if(emaildomain === `net`) {
+//         emails.net.push(user)
+//     } else if(emaildomain === `info`) {
+//         emails.info.push(user)
+//     } else {
+//         other.push(user)
+//     }
+// }
+
+
+
+for (let i = 0; i < users.length; i++) {
+    let emailDomain = users[i].email.split('.').pop(); 
+  
+    switch (emailDomain) {
+      case 'org':
+        emails.org.push(users[i]);
+        break;
+      case 'net':
+        emails.net.push(users[i]);
+        break;
+      case 'info':
+        emails.info.push(users[i]);
+        break;
+      default:
+        other.push(users[i]);
+        break;
+    }
+  }
+  
+  
+
+console.log(emails, other);
+
+
+
+
+
+
